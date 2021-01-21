c075f7c44ac44285d2a75e2633b08098679b8f74
JDForest460 <46009704+JDForest460@users.noreply.github.com>
2021-01-21 00:55:06 -0500

    set up move and jump

---
+++ Assets/Script/Player_Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpforce;
+
+
+
+    void FixedUpdate()
+    {
+        movement();
+    }
+    
+    void movement()
+	{
+        float horizontal_move = Input.GetAxis("Horizontal");
+        float face_direction = Input.GetAxisRaw("Horizontal");
+        
+
+        if(horizontal_move != 0)
+		{
+            rb.velocity = new Vector2(horizontal_move * speed * Time.deltaTime, rb.velocity.y);
+		}
+        if(face_direction != 0)
+		{
+            transform.localScale = new Vector3(face_direction, 1, 1);
+		}
+		if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+        }
+	}
+}
